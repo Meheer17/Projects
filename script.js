4d8bf7f76f7f14f13c006315fbe7b1347bae77bc
Meheer17 <75373216+Meheer17@users.noreply.github.com>
2021-08-27 20:31:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+$(window).on('scroll', function(){
+if($(window).scrollTop()){
+  $('header').addClass('nav-show');
+} else {
+  $('header').removeClass('nav-show');
+}
+})
+
+const navSlide = () => {
+	 const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".nav-bar");
+	 const navLinks = document.querySelectorAll(".nav-bar li");
+
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+ 1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+	 
+	}
+
+window.onload = () => navSlide();
+
+const body = document.querySelectorAll("body")[0];
+const blocks = document.querySelectorAll("main div");
+
+body.onscroll = function(){
+	blocks.forEach( block => {
+		if (document.documentElement.scrollTop >= block.offsetTop - 0.66 * window.screen.height && document.documentElement.scrollTop !== 0) {
+			block.className = "show";
+		} else {
+			block.className = "";
+		}
+	});
+}
+
